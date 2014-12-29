34a3a8c023236279de3d3dca479634aa390adccc
Caleb <caleb@whataloadofrubbish.com>
2014-12-28 21:29:12 -0600

    init commit for exercise 5.2, includes starter interface, class file w empty methods, and empty test class

---
+++ src/Exercise5_2/Exercise5_2.java
@@ -0,0 +1,6 @@
+package Exercise5_2;
+public class Exercise5_2{
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/Exercise5_2/SimpleMapImpl.java
@@ -0,0 +1,27 @@
+package Exercise5_2;
+public class SimpleMapImpl implements SimpleMap{
+
+	@Override
+	public String put(int key, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] get(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(int key, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}	
+}
