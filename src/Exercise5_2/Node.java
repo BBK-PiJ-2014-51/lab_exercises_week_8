52766fe8c174f5e4bfb7989809fd6f7ac28aecdd
Caleb <caleb@whataloadofrubbish.com>
2014-12-28 21:42:34 -0600

    added node class for buckets

---
+++ src/Exercise5_2/Node.java
@@ -0,0 +1,35 @@
+package Exercise5_2;
+public class Node{
+	private int key;
+	private String name;
+	private Node next;
+	
+	public Node(int key, String name){
+		this.key = key;
+		this.name = name;
+	}
+	
+	public int getKey(){
+		return key;
+	}
+	
+	public void setKey(int key){
+		this.key = key;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public Node getNext(){
+		return next;
+	}
+	
+	public void setNext(Node node){
+		next = node;
+	}
+}
