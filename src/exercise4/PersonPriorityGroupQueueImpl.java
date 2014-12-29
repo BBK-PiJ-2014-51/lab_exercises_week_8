7feb3e21864c55f2cdc36f4ebd2c55a135db94ed
Caleb <caleb@whataloadofrubbish.com>
2014-12-28 20:48:38 -0600

    implemeneted priority queue for exercise 4.2

--- src/exercise4/PersonPriorityGroupQueueImpl.java
+++ src/exercise4/PersonPriorityGroupQueueImpl.java
@@ -3,7 +3,9 @@ package exercise4;
 public class PersonPriorityGroupQueueImpl implements PersonQueue {
 	private Person[] pq = new Person[10];
 	private int size = 0;
-	
+	private final static int OLD_AGE = 65;
+	private final static int YOUNG_AGE = 18;
+	private final static int MIN_AGE = -1;
 	@Override
 	public void insert(Person person) {
 		if (size == pq.length) expandArray();
@@ -12,7 +14,10 @@ public class PersonPriorityGroupQueueImpl implements PersonQueue {
 
 	@Override
 	public Person retrieve() {
-		return pq[--size];
+		if (hasPersonOverAge(OLD_AGE)) return getNextPersonOverAge(OLD_AGE);
+		else if (hasPersonOverAge(YOUNG_AGE)) return getNextPersonOverAge(YOUNG_AGE);
+		else if (hasPersonOverAge(MIN_AGE)) return getNextPersonOverAge(MIN_AGE);
+		else return null;
 	}
 	
 	private void expandArray() {
@@ -21,6 +26,31 @@ public class PersonPriorityGroupQueueImpl implements PersonQueue {
 			newPq[i] = pq[i];
 		}
 		pq = newPq;
+	}
+	
+	private boolean hasPersonOverAge(int age){
+		boolean hasAge = false;
+		for (int i = 0; i < size; i++){
+			if (pq[i].getAge() > age) hasAge = true;
+		}
+		return hasAge;
+	}
+	
+	private Person getNextPersonOverAge(int age){
+		Person firstPersonOfAge = null;
+		int firstPersonOfAgeIndex = -1;
+		for (int i = size - 1; i >= 0; i--){
+			if (pq[i].getAge() > age){
+				firstPersonOfAge = pq[i];
+				firstPersonOfAgeIndex = i;
+			}
+		}
+		
+		size--;
+		for (int i = firstPersonOfAgeIndex; i < size; i++){
+			pq[i] = pq[i + 1];
+		}
+		return firstPersonOfAge;
 	}
 
 }
